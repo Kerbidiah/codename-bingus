1df40f824451d461a100f6e64ac89ef9e0f76171
Alex Janninck <alexjanninck@gmail.com>
2026-07-07 01:03:57 +0200

    Fixed item.rs
    turns out .to_string() isn't const (has to do with dynamic length of string and the fact that a heap allocation can't be const.

--- bingus/src-tauri/src/bingo/item.rs
+++ bingus/src-tauri/src/bingo/item.rs
@@ -15,33 +15,35 @@ pub struct BingoItem {
 }
 
 impl BingoItem {
-	const VIENNA_SAMPLES: [Self; 3] = [
-		// 🌭 Eat a Käsekrainer or a döner / falafel wrap
-		Self {
-			title: "Eat a Käsekrainer",
-			emoji: Option<'🌭'>,
-			short_description: Some(String::from("or a döner / falafel wrap")),
-			long_description: None,
-			url: Some(String::from("https://www.wien.info/en/dine-drink/viennese-cuisine/hot-dog-stands-348128")),
-			completion_info: None,
-		},
-		// 🎻 Find the golden Strauss
-		Self {
-			title: "Find the golden Strauss",
-			emoji: Option<'🎻'>,
-			short_description: None,
-			long_description: None,
-			url: Some(String::from("https://www.wien.info/en/art-culture/music-stage-shows/strauss-monument-360274")),
-			completion_info: None,
-		},
-		// 🫙 Sample food at the Brunnenmarkt
-		Self {
-			title: "Sample food at the Brunnenmarkt",
-			emoji: Option<'🫙'>,
-			short_description: None,
-			long_description: None,
-			url: Some(String::from("https://www.wien.info/en/dine-drink/markets/yppenplatz-restaurants-366538")),
-			completion_info: None,
-		},
-	]
+	pub fn vienna_samples() -> [Self; 3] {
+		[
+			// 🌭 Eat a Käsekrainer or a döner / falafel wrap
+			Self {
+				title: "Eat a Käsekrainer".to_string(),
+				emoji: Some('🌭'),
+				short_description: Some("or a döner / falafel wrap".to_string()),
+				long_description: None,
+				url: Some("https://www.wien.info/en/dine-drink/viennese-cuisine/hot-dog-stands-348128".to_string()),
+				completion_info: None,
+			},
+			// 🎻 Find the golden Strauss
+			Self {
+				title: "Find the golden Strauss".to_string(),
+				emoji: Some('🎻'),
+				short_description: None,
+				long_description: None,
+				url: Some("https://www.wien.info/en/art-culture/music-stage-shows/strauss-monument-360274".to_string()),
+				completion_info: None,
+			},
+			// 🫙 Sample food at the Brunnenmarkt
+			Self {
+				title: "Sample food at the Brunnenmarkt".to_string(),
+				emoji: Some('🫙'),
+				short_description: None,
+				long_description: None,
+				url: Some("https://www.wien.info/en/dine-drink/markets/yppenplatz-restaurants-366538".to_string()),
+				completion_info: None,
+			},
+		]
+	}
 }
