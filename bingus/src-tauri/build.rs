bcd2a4d58ad9d8b5c780d7f46603ec86ceeda135
Alex Janninck <alexjanninck@gmail.com>
2026-07-06 22:18:10 +0200

    fixed tabs and some other setup stuff

--- bingus/src-tauri/build.rs
+++ bingus/src-tauri/build.rs
@@ -1,3 +1,10 @@
-fn main() {
-    tauri_build::build()
+fn main() -> std::io::Result<()> {
+	prost_build::compile_protos(
+		&["proto/bingo_item.proto", "proto/bingo_board.proto"],
+		&["proto/"], // include path, so `import "bingo_item.proto"` resolves
+	)?;
+
+	tauri_build::build();
+
+	Ok(())
 }
--- bingus/src-tauri/src/main.rs
+++ bingus/src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    bingus_lib::run()
+	bingus_lib::run()
 }
