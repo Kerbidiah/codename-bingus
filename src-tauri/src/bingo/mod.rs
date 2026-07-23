8b669913e828886f099c3c0e76e7d49ea246ce81
Alex Janninck <alexjanninck@gmail.com>
2026-07-23 12:54:44 +0200

    Adjust get_bingo_games to return path

--- src-tauri/src/bingo/mod.rs
+++ src-tauri/src/bingo/mod.rs
@@ -62,14 +62,31 @@ pub fn get_bingo_projects() -> Vec<(BingoProject, String)> {
 }
 
 #[tauri::command]
-pub fn get_bingo_games() -> Vec<PlayableBingo> {
+pub fn get_bingo_games() -> Vec<(PlayableBingo, String)> {
 	let path_play = resolve_path(BINGO_PLAY_PATH);
 
 	fs::read_dir(path_play)
 		.unwrap()
-		.filter_map(|res| res.ok().map(|dir| File::open(dir.path())))
-		.filter_map(|f| f.ok().map(|x| x)) // filter out Errors and extract the value out of Oks
-		.map(|mut f| PlayableBingo::from_file(&mut f))
-		.filter_map(|f| f.ok().map(|x| x)) // filter out Errors and extract the value out of Oks
+		.filter_map(|res| {
+			res.ok().map(|dir| {
+				let p = dir.path();
+				(File::open(&p), p.to_str().unwrap().to_string())
+			})
+		})
+		.filter_map(|f| {
+			if f.0.is_ok() {
+				Some((f.0.unwrap(), f.1))
+			} else {
+				None
+			}
+		}) // filter out Errors and extract the value out of Oks
+		.map(|mut f| (PlayableBingo::from_file(&mut f.0), f.1))
+		.filter_map(|f| {
+			if f.0.is_ok() {
+				Some((f.0.unwrap(), f.1))
+			} else {
+				None
+			}
+		}) // filter out Errors and extract the value out of Oks
 		.collect()
 }
