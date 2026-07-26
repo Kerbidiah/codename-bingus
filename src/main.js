95123f07bca50dae7a78ed75397bb1a9bd601a9c
Zayaan Saleem <zsaleem8@gmu.edu>
2026-07-26 15:29:49 +0200

    S4: Factored out theme changer into themes.js. Started play board screen

--- src/main.js
+++ src/main.js
@@ -1,43 +1,6 @@
 const { invoke } = window.__TAURI__.core;
 
-const toggle = document.getElementById("dark-mode");
-const theme_img = document.getElementById("theme-img");
-let editable_img;
 const cards = document.getElementById("bingo-cards");
-
-const ICON_LIGHT = "/assets/enable-light-mode.svg";
-const ICON_DARK = "/assets/enable-dark-mode.svg";
-
-// Changes the theme depending on the theme the user has
-function setTheme(theme) {
-	editable_img = Array.from(document.getElementsByClassName("edit-button"));
-	document.body.classList.toggle("light", theme === "light");
-	theme_img.src = theme === "light" ? ICON_DARK : ICON_LIGHT;
-	if (editable_img != undefined) {
-		editable_img.forEach((img) => {
-			const dark_icon = "/assets/editable-board-dark.svg";
-			const light_icon = "/assets/editable-board-light.svg";
-			img.src = theme === "light" ? light_icon : dark_icon;
-		});
-	}
-	localStorage.setItem("theme", theme);
-}
-// Caches the user's theme preference
-const saved = localStorage.getItem("theme");
-if (saved === "light" || saved === "dark") {
-	setTheme(saved);
-} else {
-	const prefersLight = window.matchMedia(
-		"(prefers-color-scheme: light)",
-	).matches;
-	setTheme(prefersLight ? "light" : "dark");
-}
-
-toggle.addEventListener("click", (e) => {
-	e.preventDefault(); // prevents jumping due to href
-	const next = document.body.classList.contains("light") ? "dark" : "light";
-	setTheme(next);
-});
 
 // Creates the card in the DOM
 function draw_card(title, city, path, owner) {
@@ -69,7 +32,7 @@ function draw_card(title, city, path, owner) {
 		const edit_anchor = document.createElement("a");
 		const edit_img = document.createElement("img");
 		const EDIT_ICON_PATH =
-			saved === "light"
+			localStorage.getItem('theme') === "light"
 				? "/assets/editable-board-light.svg"
 				: "/assets/editable-board-dark.svg";
 		edit_img.src = EDIT_ICON_PATH;
