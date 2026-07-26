59d4976bfeb27e630926d7276d441cb4fa6aa13b
Alex Janninck <alexjanninck@gmail.com>
2026-07-26 16:13:51 +0200

    minor changes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,15 +21,15 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-	let bingo_proj = BingoProject {
-		title: "TEST".to_string(),
-		city: "Fuckin Linz Man".to_string(),
-		items: BingoItem::vienna_samples().unwrap(),
-		last_board: None,
-	};
+	// let bingo_proj = BingoProject {
+	// 	title: "TEST".to_string(),
+	// 	city: "Fuckin Linz Man".to_string(),
+	// 	items: BingoItem::vienna_samples().unwrap(),
+	// 	last_board: None,
+	// };
 
-	let mut f = File::create("../examples/projects/TEST.BingoProject").unwrap();
-	bingo_proj.to_file(&mut f).unwrap();
+	// let mut f = File::create("../examples/projects/TEST.BingoProject").unwrap();
+	// bingo_proj.to_file(&mut f).unwrap();
 
 	tauri::Builder::default()
 		.plugin(tauri_plugin_opener::init())
