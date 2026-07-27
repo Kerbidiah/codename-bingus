3b97343ef0cbae8f2a96e6ea9748cc0c47fa7327
devinpbell <devinpbell@gmail.com>
2026-07-27 18:24:08 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -86,6 +86,13 @@ if (
 		});
 		cards.appendChild(create_board_button);
 	} else {
+		const create_board_button = document.createElement("button");
+		create_board_button.id = "create-board";
+		create_board_button.textContent = "Create Board";
+		create_board_button.addEventListener("click", () => {
+			window.location.href = "editable-board/editable-board.html";
+		});
+		cards.appendChild(create_board_button);
 		if (editable_boards != undefined) {
 			for (const elem of editable_boards) {
 				const [items, path] = elem;
