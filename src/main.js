3ab941ae3cd7ecc1dd019486d1042ad5b8439b4a
Zayaan Saleem <zsaleem8@gmu.edu>
2026-07-27 17:36:39 +0200

    S4: Refreshes page keeps scroll position upon exporting

--- src/main.js
+++ src/main.js
@@ -115,5 +115,8 @@ if (
 				window.location.href = "./play-board/play-board.html";
 			}
 		}
+		const position = window.scrollY;
+		window.location.reload();
+		window.scrollTo(0, position);
 	});
 }
