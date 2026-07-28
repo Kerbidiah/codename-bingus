4a1cd7b0d309e3c4cdbb89ca73f334df6f8f39db
Zayaan Saleem <zsaleem8@gmu.edu>
2026-07-28 21:23:23 +0200

    S4: Remove script from HTML for exporting. Added file dialog dependency.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -46,6 +46,7 @@ pub fn run() {
 	}
 
 	tauri::Builder::default()
+		.plugin(tauri_plugin_dialog::init())
 		.plugin(tauri_plugin_opener::init())
 		.invoke_handler(tauri::generate_handler![
 			generate_dummy_bingo_board,
